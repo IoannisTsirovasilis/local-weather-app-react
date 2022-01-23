003db3ded73dbbd2e9474716129bbed8ed578b47
Ioannis Tsirovasilis <ioannistsirovasilis@gmail.com>
2022-01-23 13:58:13 +0200

    Extract weekdays to a constant

---
+++ src/common/constants.js
@@ -0,0 +1,11 @@
+const WEEKDAYS = {
+  SUNDAY: { long: 'Sunday', short: 'Sun', index: 0 },
+  MONDAY: { long: 'Monday', short: 'Mon', index: 1 },
+  TUESDAY: { long: 'Tuesday', short: 'Tue', index: 2 },
+  WEDNESDAY: { long: 'Wednesday', short: 'Wed', index: 3 },
+  THURSDAY: { long: 'Thursday', short: 'Thu', index: 4 },
+  FRIDAY: { long: 'Friday', short: 'Fri', index: 5 },
+  SATURDAY: { long: 'Saturday', short: 'Sat', index: 6 },
+};
+
+export default WEEKDAYS;
