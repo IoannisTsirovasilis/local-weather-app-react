d16d74d35d27880fcf3b8afe7b69e3c802449ea5
Ioannis Tsirovasilis <ioannistsirovasilis@gmail.com>
2022-01-23 14:01:04 +0200

    Add day header at daily weather container

--- src/components/daily-weather-container/daily-weather-container.js
+++ src/components/daily-weather-container/daily-weather-container.js
@@ -1,5 +1,19 @@
+import DayHeader from '../day-header/day-header';
+import WEEKDAYS from '../../common/constants';
+import './daily-weather-container.css'
+
+function getTodayIndex() {
+  return new Date().getDay();
+}
+
+function getTodayLongName() {
+  return Object.values(WEEKDAYS).find((wd) => wd.index === getTodayIndex()).long;
+}
+
 function DailyWeatherContainer() {
-  return <div></div>;
+  return <div className='daily-weather-container'>
+    <DayHeader day={getTodayLongName()} />
+  </div>;
 }
 
 export default DailyWeatherContainer;
