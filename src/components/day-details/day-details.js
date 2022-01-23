bee25d35e0698f5fa97fe9ff83870e67e498f367
Ioannis Tsirovasilis <ioannistsirovasilis@gmail.com>
2022-01-23 16:53:34 +0200

    Update day header component
    Rename day header component to day details
    Turn h1 to div
    Add font-size equals to 6vw

---
+++ src/components/day-details/day-details.js
@@ -0,0 +1,7 @@
+import './day-details.css'
+
+function DayDetails(props) {
+  return <div className='day-details'>{props.day}</div>
+}
+
+export default DayDetails;
--- src/components/day-header/day-header.js
+++
@@ -1,5 +0,0 @@
-function DayHeader(props) {
-  return <h1>{props.day}</h1>
-}
-
-export default DayHeader;
