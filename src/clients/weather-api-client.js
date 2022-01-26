074d118f75ce6f171583cff4986d1b9f3172b81a
Ioannis Tsirovasilis <ioannistsirovasilis@gmail.com>
2022-01-26 15:27:44 +0200

    Add weather-api-client.js
    Fetch weather info from API
    TODO fetch location from user's actual location

---
+++ src/clients/weather-api-client.js
@@ -0,0 +1,23 @@
+import axios from "axios"
+import constants from "../common/constants"
+
+
+const lat = 38.012630;
+const lon = 23.820551;
+
+export async function fetchWeather(dispatch) {
+  try {
+    const response = await axios.get(`${process.env.REACT_APP_WEATHER_API_URL}/weather-app?lat=${lat}&lon=${lon}`);
+    const data = {
+      ...response.data
+    }
+
+    data.current.weather[0].main = constants.OPEN_WEATHER_API_WEATHER_CONDITION_MAP[data.current.weather[0].main];
+    data.daily.forEach(day => {
+      day.weather[0].main = constants.OPEN_WEATHER_API_WEATHER_CONDITION_MAP[day.weather[0].main];
+    }); 
+    dispatch({ type: 'weather-api/success', payload: data }) 
+  } catch (error) {
+    dispatch({ type: 'weather-api/error', payload: error })
+  }
+}
